1fc591fcf1ee9f80beb4039c04ac6fa97a68e220
Anna Vitova <avitova@redhat.com>
2023-03-17 11:07:57 +0100

    Show processing result

--- app/controllers/template_invocations_controller.rb
+++ app/controllers/template_invocations_controller.rb
@@ -8,13 +8,15 @@ class TemplateInvocationsController < ApplicationController
   def show
     @template_invocation = TemplateInvocation.find(params[:id])
     raise ActiveRecord::RecordNotFound unless User.current.can?(:view_job_invocations, @template_invocation.job_invocation)
-
     @template_invocation_task = @template_invocation.run_host_job_task
+
     @host = @template_invocation.host
     @auto_refresh = @template_invocation_task.pending?
     @since = params[:since].to_f if params[:since].present?
     @line_sets = @template_invocation_task.main_action.live_output
     @line_sets = @line_sets.drop_while { |o| o['timestamp'].to_f <= @since } if @since
+    @template_output_sets = @line_sets.select {|o| o['output_type'] == 'template_output'}
+    @line_sets.select! {|o| o['output_type'] != 'template_output'}
     @line_counter = params[:line_counter].to_i
   end
 end
