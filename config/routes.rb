456adae14a5d5bb71d17ef1b2a3185f45792138b
Anna Vitova <avitova@redhat.com>
2023-04-02 10:31:45 +0200

    Delete unused output template routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # This file
   resources :job_templates, :except => [:show] do
     member do
       get 'clone_template'
@@ -68,8 +69,6 @@ Rails.application.routes.draw do
   get 'ui_job_wizard/template/:id', to: 'ui_job_wizard#template'
   get 'ui_job_wizard/resources', to: 'ui_job_wizard#resources'
   get 'ui_job_wizard/job_invocation', to: 'ui_job_wizard#job_invocation'
-  get 'ui_job_wizard/output_templates', to: 'ui_job_wizard#output_templates'
-  get 'ui_job_wizard/output_template/:id', to: 'ui_job_wizard#output_template'
 
   namespace :api, :defaults => {:format => 'json'} do
     scope '(:apiv)', :module => :v2, :defaults => {:apiv => 'v2'}, :apiv => /v1|v2/, :constraints => ApiConstraints.new(:version => 2, :default => true) do
